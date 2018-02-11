085733468a4552dc15deb72b3dda1442c37e5e11
Joshua Loschen <josh.loschen@gmail.com>
2018-02-11 15:36:12 -0600

    Added FlatHand images. Got first placement working.

--- js/app.js
+++ js/app.js
@@ -3,38 +3,57 @@ var $greenHand;
 var $text1; 
 var height = 13042;
 var distanceToCenter = 0;
+var midScreenY = 0;
+
+function setCenter(){
+    distanceToCenter = $win.width() / 2;
+    midScreenY = $win.height() / 2;
+}
 
 $(function(){
-    // console.log('ready!');
     $greenHand = $('#greenHand');
     $text1 = $('#text1');  
-    distanceToCenter = $win.width() / 2;
-    console.log(distanceToCenter);
+    setCenter();
 });
 
 // https://www.youtube.com/watch?v=nhHqiGCG10E
 
 $win.on('scroll', function(){
+    animate();
+});
+
+function animate(){
     var distance = $win.scrollTop();
 
-    console.log('distance ' + distance);
-    
     //move hand
     if(distance < 1000){
         //var left = parseInt($greenHand.css('left')) + 2;
         //var left = distanceToCenter - distance;
         var left = (distanceToCenter + 100) + (1000 - distance); 
         $greenHand.css('left', left + 'px');
-        //console.log(left);
-    }else if(distance < 2000){
+        $text1.css('left', (left - 500) + 'px');
+
+        var top = (midScreenY) + (500 - distance / 2);
+        $greenHand.css('top', top + 'px');
+        $text1.css('top', (top +20) + 'px');
+    }else if(distance < 2600){
         var left = (distanceToCenter + 100) + (distance - 1000);
         $greenHand.css('left', left + 'px');
+
+        var top = (midScreenY) + (distance / 2 - 500);
+        $greenHand.css('top', top + 'px');
+
+
+        //var top2 = (midScreenY) + (500 - distance / 3);
+        //var left = (distanceToCenter + 100) + (1000 - distance); 
+        $text1.css('left', (distanceToCenter - 400) + 'px');
+        
+        var overLimit = distance - 1000;
+        var top2 = (midScreenY) - (overLimit / 3);
+        $text1.css('top', (top2 + 20)  + 'px');
     }
-    
-    //console.log($greenHand);
-    //console.log(left);
-});
+}
 
 $win.on('resize', function(){
-    distanceToCenter = $win.width() / 2;
+    setCenter();
 });
