cf98f8cab48e4899f8aed63da555b9b5fd18e290
Joshua Loschen <josh.loschen@gmail.com>
2018-02-12 08:22:57 -0600

    Added Orange Hands

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var $win = $(window);
-var $greenHand, $blueHand; 
-var $text1, $text2, $text3;
+var $greenHand, $blueHand, $orangeLeft, $orangeRight; 
+var $text1, $text2, $text3, $text4;
 
 var height = 13042;
 var distanceToCenter = 0;
@@ -14,10 +14,13 @@ function setCenter(){
 $(function(){
     $greenHand = $('#greenHand');
     $blueHand = $('#blueHand');
+    $orangeLeft = $('#orange-left');
+    $orangeRight = $('#orange-right');
 
     $text1 = $('#text1');
     $text2 = $('#text2');
     $text3 = $('#text3');
+    $text4 = $('#text4');
     setCenter();
 });
 
@@ -28,9 +31,12 @@ $win.on('scroll', function(){
 
 function animate(){
     var distance = $win.scrollTop();
-    //console.log(distance);
+    //distance = 2800;
+    var halfDistance = distance / 2;
+
+    var thirdDistance = distance / 3;
+    
     //move pinch hand
-    //distance = 1500;
     if(distance < 1000){
         var left = (distanceToCenter) + (1100 - distance); 
         
@@ -38,7 +44,7 @@ function animate(){
         
         $left($text1, left - 430);
 
-        var top = (midScreenY) + (500 - distance / 2);
+        var top = (midScreenY) + (500 - halfDistance);
         $top($greenHand, top );
         
         $top($text1, top + 20);
@@ -46,7 +52,7 @@ function animate(){
     }else /*if(distance < 2600)*/{
         $left($greenHand, distanceToCenter + (distance - 810));
 
-        $top($greenHand, (midScreenY) + (distance / 2 - 520) + 20)
+        $top($greenHand, (midScreenY) + (halfDistance - 520) + 20)
 
         $left($text1, distanceToCenter - 330);
         
@@ -55,7 +61,7 @@ function animate(){
 
     //blue hand
     if(distance < 1500){
-        var top = (midScreenY) + (750 - distance / 2);
+        var top = (midScreenY) + (750 - halfDistance);
         $top($blueHand, top);
         $top($text2, top - 50);
 
@@ -63,7 +69,7 @@ function animate(){
         $left($text2, distanceToCenter - 330);
     }else {
         $top($text2, (midScreenY) - ((distance - 1000) / 3) + 117);
-        $top($blueHand, (midScreenY) + (distance / 2 - 750));
+        $top($blueHand, (midScreenY) + (halfDistance - 750));
 
         var left = distanceToCenter - 50; 
         $left($blueHand, left);
@@ -77,6 +83,44 @@ function animate(){
     if(1500 < distance && distance < 2300){
         var percent = (distance - 1500) / 800;
         $text3.css('opacity', percent);
+    }
+
+
+    // $top($text4, (midScreenY) - ((distance - 1000) / 3) + 297);
+    // $left($text4, distanceToCenter - 220);
+
+    //orange hands
+    if(distance < 2800){
+        //var top = (midScreenY) + (750 - halfDistance) + 350;
+        var top = midScreenY + 1100 - halfDistance;
+        $top($orangeLeft, top);
+        $top($orangeRight, top);
+        $top($text4, top + 80);
+
+        $left($orangeLeft, distanceToCenter - 640);
+        $left($orangeRight, distanceToCenter + 150);
+        $left($text4, distanceToCenter - 170);
+    }else {
+        var newTop = (midScreenY) + (halfDistance - 1700);
+        $top($orangeLeft, newTop);
+        $top($orangeRight, newTop);
+
+        $top($text4, (midScreenY) - ((distance - 1000) / 3) + 381);
+        
+
+        //var overLimit = distance - 2800;
+        //var overLimit = halfDistance - 1400;
+        var overLimit = thirdDistance - 933;
+        //distanceToCenter + (distance - 810));
+        $left($orangeLeft, distanceToCenter - (640 + overLimit));
+        //$left($orangeRight, distanceToCenter + (distance - 810));
+        $left($orangeRight, distanceToCenter + 150 + overLimit);
+        
+        $left($text4, distanceToCenter - 170);
+        // var left = distanceToCenter - 50; 
+        // $left($orangeLeft, left);
+        // $left($orangeRight, left);
+        //$left($text4, left - 280);
     }
 }
 
